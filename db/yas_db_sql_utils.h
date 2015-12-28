75c1cab0865bfec33840e297f2d19146e99857ff
Yuki Yasoshima <yukiyasos@gmail.com>
2015-12-28 12:34:30 +0900

    add sql_utils

---
+++ db/yas_db_sql_utils.h
@@ -0,0 +1,20 @@
+//
+//  yas_sql_utils.h
+//
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace yas {
+namespace db {
+    std::string insert_sql(std::string const &table, std::vector<std::string> const &fields);
+    std::string update_sql(std::string const &table, std::vector<std::string> const &fields, std::string const &where);
+    std::string delete_sql(std::string const &table, std::string const &where);
+
+    std::string expr(std::string const &field, std::string const &op);
+    std::string equal_expr(std::string const &field);
+    std::string joined_exprs(std::vector<std::string> const &fields);
+}
+}
---
+++ db_test/db_mac_tests/yas_db_sql_utils_tests.mm
@@ -0,0 +1,42 @@
+//
+//  yas_db_sql_utils.mm
+//
+
+#import "yas_db_test_utils.h"
+
+@interface yas_db_sql_utils : XCTestCase
+
+@end
+
+@implementation yas_db_sql_utils
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)test_equal_expr {
+    XCTAssertEqual(yas::db::equal_expr("abc"), "abc = :abc");
+}
+
+- (void)test_joined_exprs {
+    XCTAssertEqual(yas::db::joined_exprs({"abc", "def"}), "abc = :abc and def = :def");
+}
+
+- (void)test_insert_sql {
+    XCTAssertEqual(yas::db::insert_sql("aaa", {"abc", "def"}), "insert into aaa(abc, def) values(:abc, :def);");
+}
+
+- (void)test_update_sql {
+    XCTAssertEqual(yas::db::update_sql("ccc", {"qwe", "rty"}, "uio = :uio"),
+                   "update ccc set qwe = :qwe, rty = :rty where uio = :uio;");
+}
+
+- (void)test_delete_sql {
+    XCTAssertEqual(yas::db::delete_sql("bbb", "xyz = :xyz"), "delete from bbb where xyz = :xyz;");
+}
+
+@end
