016d344a5ed17c1f977b712a4d5dae9900153d73
Yuki Yasoshima <yukiyasos@gmail.com>
2015-12-29 23:01:40 +0900

    update range

--- db/yas_db_range.cpp
+++ db/yas_db_range.cpp
@@ -9,6 +9,15 @@ using namespace yas;
 db::range::range(UInt64 const location, UInt64 const length) : location(location), length(length) {
 }
 
+bool db::range::is_empty() const {
+    return length == 0;
+}
+
 std::string db::range::sql() const {
     return std::to_string(location) + ", " + std::to_string(length);
+}
+
+const db::range &db::range::empty() {
+    static range const _empty_range = db::range{0, 0};
+    return _empty_range;
 }
