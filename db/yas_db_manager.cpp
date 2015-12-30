9e555d2861e1e6b88a852c7385ff30551168f8bc
Yuki Yasoshima <yukiyasos@gmail.com>
2015-12-30 23:03:33 +0900

    update manager

--- db/yas_db_manager.cpp
+++ db/yas_db_manager.cpp
@@ -7,12 +7,32 @@
 using namespace yas;
 
 struct db::manager::impl : public base::impl {
-    impl(std::string const &path) {
+    database database;
+    operation_queue queue;
+
+    impl(std::string const &path) : database(path), queue() {
+        database.open();
     }
 };
 
-db::manager::manager(std::string const &path) : super_class(std::make_shared<impl>(path)) {
+db::manager::manager(std::string const &path) : super_class(std::make_unique<impl>(path)) {
 }
 
 db::manager::manager(std::nullptr_t) : super_class(nullptr) {
+}
+
+std::string const &db::manager::database_path() const {
+    return impl_ptr<impl>()->database.database_path();
+}
+
+void db::manager::execute(execution_f &&db_execution) {
+    auto ip = impl_ptr<impl>();
+
+    auto execution = [db_execution = std::move(db_execution), db = ip->database](operation const &op) mutable {
+        if (!op.is_canceled()) {
+            db_execution(db, op);
+        }
+    };
+
+    ip->queue.add_operation(operation{std::move(execution)});
 }
