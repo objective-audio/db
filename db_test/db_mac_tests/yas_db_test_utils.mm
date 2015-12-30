b41941b0dec11f0a346ab2126af1bf96a2fa8e24
Yuki Yasoshima <yukiyasos@gmail.com>
2015-12-30 23:04:01 +0900

    update test_utils

--- db_test/db_mac_tests/yas_db_test_utils.mm
+++ db_test/db_mac_tests/yas_db_test_utils.mm
@@ -12,6 +12,16 @@
     return yas::db::database{db_path};
 }
 
++ (yas::db::manager)create_test_manager {
+    NSString *databasePath = [[self class] databasePath];
+    std::string db_path = yas::to_string((__bridge CFStringRef)databasePath);
+    return yas::db::manager{db_path};
+}
+
++ (std::string)database_path {
+    return yas::to_string((__bridge CFStringRef)[self databasePath]);
+}
+
 + (NSString *)databasePath {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *dir = [paths objectAtIndex:0];
