a4ad485a2d184e71bcecca394fc6c0729e7e1df5
Yuki Yasoshima <yukiyasos@gmail.com>
2016-01-09 00:00:56 +0900

    update sql_utils_tests

--- db_test/db_mac_tests/yas_db_sql_utils_tests.mm
+++ db_test/db_mac_tests/yas_db_sql_utils_tests.mm
@@ -4,6 +4,8 @@
 
 #import "yas_db_test_utils.h"
 
+using namespace yas;
+
 @interface yas_db_sql_utils : XCTestCase
 
 @end
@@ -19,51 +21,48 @@
 }
 
 - (void)test_create_table_sql {
-    XCTAssertEqual(yas::db::create_table_sql("test_table", {"field_a", "field_b"}),
+    XCTAssertEqual(db::create_table_sql("test_table", {"field_a", "field_b"}),
                    "create table if not exists test_table (field_a, field_b);");
 }
 
 - (void)test_alter_table_sql {
-    XCTAssertEqual(yas::db::alter_table_sql("test_table", "field_a"), "alter table test_table add column field_a;");
+    XCTAssertEqual(db::alter_table_sql("test_table", "field_a"), "alter table test_table add column field_a;");
 }
 
 - (void)test_drop_table_sql {
-    XCTAssertEqual(yas::db::drop_table_sql("test_table"), "drop table if exists test_table;");
+    XCTAssertEqual(db::drop_table_sql("test_table"), "drop table if exists test_table;");
 }
 
 - (void)test_insert_sql {
-    XCTAssertEqual(yas::db::insert_sql("aaa", {"abc", "def"}), "insert into aaa(abc, def) values(:abc, :def);");
-    XCTAssertEqual(yas::db::insert_sql("bbb"), "insert into bbb default values;");
+    XCTAssertEqual(db::insert_sql("aaa", {"abc", "def"}), "insert into aaa(abc, def) values(:abc, :def);");
+    XCTAssertEqual(db::insert_sql("bbb"), "insert into bbb default values;");
 }
 
 - (void)test_update_sql {
-    std::cout << yas::db::update_sql("ccc", {"qwe", "rty"}, "(uio = :uio)") << std::endl;
-    XCTAssertEqual(yas::db::update_sql("ccc", {"qwe", "rty"}, "(uio = :uio)"),
-                   "update ccc set (qwe = :qwe), (rty = :rty) where (uio = :uio);");
+    XCTAssertEqual(db::update_sql("ccc", {"qwe", "rty"}, "(uio = :uio)"),
+                   "update ccc set qwe = :qwe, rty = :rty where (uio = :uio);");
 }
 
 - (void)test_delete_sql {
-    XCTAssertEqual(yas::db::delete_sql("bbb", "xyz = :xyz"), "delete from bbb where xyz = :xyz;");
+    XCTAssertEqual(db::delete_sql("bbb", "xyz = :xyz"), "delete from bbb where xyz = :xyz;");
 }
 
 - (void)test_expr {
-    XCTAssertEqual(yas::db::expr("abc", ":def", "="), "(abc = :def)");
+    XCTAssertEqual(db::expr("abc", ":def", "="), "(abc = :def)");
 }
 
 - (void)test_field_expr {
-    XCTAssertEqual(yas::db::field_expr("abc", "="), "(abc = :abc)");
+    XCTAssertEqual(db::field_expr("abc", "="), "(abc = :abc)");
 }
 
 - (void)test_joined_orders {
-    auto joined_orders =
-        yas::db::joined_orders({{"field_a", yas::db::order::ascending}, {"field_b", yas::db::order::descending}});
+    auto joined_orders = db::joined_orders({{"field_a", db::order::ascending}, {"field_b", db::order::descending}});
     XCTAssertEqual(joined_orders, "field_a asc, field_b desc");
 }
 
 - (void)test_select_sql {
-    auto select_sql = yas::db::select_sql(
-        "test_table", {"field_a", "field_b"}, "abc = :def",
-        {{"field_c", yas::db::order::ascending}, {"field_d", yas::db::order::descending}}, {10, 20});
+    auto select_sql = db::select_sql("test_table", {"field_a", "field_b"}, "abc = :def",
+                                     {{"field_c", db::order::ascending}, {"field_d", db::order::descending}}, {10, 20});
     XCTAssertEqual(
         select_sql,
         "select field_a, field_b from test_table order by field_c asc, field_d desc limit 10, 20 where abc = :def;");
