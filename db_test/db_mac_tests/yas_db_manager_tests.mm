6ba077fff8b5ba3bca5de1cbc04b7e3e9955dcab
Yuki Yasoshima <yukiyasos@gmail.com>
2016-03-04 16:21:21 +0900

    update tests

--- db_test/db_mac_tests/yas_db_manager_tests.mm
+++ db_test/db_mac_tests/yas_db_manager_tests.mm
@@ -1985,6 +1985,7 @@ using namespace yas;
 
 - (void)test_to_string_from_error {
     XCTAssertEqual(to_string(db::manager::error_type::begin_transaction_failed), "begin_transaction_failed");
+    XCTAssertEqual(to_string(db::manager::error_type::vacuum_failed), "vacuum_failed");
     XCTAssertEqual(to_string(db::manager::error_type::select_info_failed), "select_info_failed");
     XCTAssertEqual(to_string(db::manager::error_type::update_info_failed), "update_info_failed");
     XCTAssertEqual(to_string(db::manager::error_type::version_not_found), "version_not_found");
@@ -2000,11 +2001,14 @@ using namespace yas;
     XCTAssertEqual(to_string(db::manager::error_type::save_id_not_found), "save_id_not_found");
     XCTAssertEqual(to_string(db::manager::error_type::update_save_id_failed), "update_save_id_failed");
     XCTAssertEqual(to_string(db::manager::error_type::delete_failed), "delete_failed");
+    XCTAssertEqual(to_string(db::manager::error_type::purge_failed), "purge_failed");
+    XCTAssertEqual(to_string(db::manager::error_type::purge_relation_failed), "purge_relation_failed");
     XCTAssertEqual(to_string(db::manager::error_type::select_last_failed), "select_last_failed");
     XCTAssertEqual(to_string(db::manager::error_type::select_revert_failed), "select_revert_failed");
     XCTAssertEqual(to_string(db::manager::error_type::fetch_object_datas_failed), "fetch_object_datas_failed");
     XCTAssertEqual(to_string(db::manager::error_type::out_of_range_save_id), "out_of_range_save_id");
     XCTAssertEqual(to_string(db::manager::error_type::select_failed), "select_failed");
+    XCTAssertEqual(to_string(db::manager::error_type::last_insert_rowid_failed), "last_insert_rowid_failed");
     XCTAssertEqual(to_string(db::manager::error_type::none), "none");
 }
 
