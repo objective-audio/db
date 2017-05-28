cdbed7702e458cfb573c69c09794a4748b1177b7
Yuki Yasoshima <yukiyasos@gmail.com>
2017-05-28 18:06:47 +0900

    add select_option_tests

---
+++ db_test/db_mac_tests/yas_db_select_option_tests.mm
@@ -0,0 +1,21 @@
+//
+//  yas_db_select_option_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+
+@interface yas_db_select_option_tests : XCTestCase
+
+@end
+
+@implementation yas_db_select_option_tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+@end
