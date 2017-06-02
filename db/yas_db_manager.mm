f38413c0a0fa98d02064f720a8034b30426ebe6d
Yuki Yasoshima <yukiyasos@gmail.com>
2017-06-02 23:40:34 +0900

    manager

--- db/yas_db_manager.mm
+++ db/yas_db_manager.mm
@@ -485,7 +485,7 @@ struct db::manager::impl : public base::impl, public object_observable::impl {
                         state = db::manager_result_t{std::move(update_result.error())};
                     }
                 } else {
-                    state = db::manager_result_t{std::move(clear_result.error())};
+                    state = std::move(clear_result);
                 }
 
                 // トランザクション終了
@@ -519,17 +519,15 @@ struct db::manager::impl : public base::impl, public object_observable::impl {
             // トランザクション開始
             if (auto begin_result = db::begin_transaction(db)) {
                 if (auto purge_result = db::purge_db(db, model)) {
-                    db_info = std::move(purge_result.value());
+                    // infoをクリア。セーブIDを1にする
+                    db::value const one_value = db::value{db::integer::type{1}};
+                    if (auto update_result = db::update_db_info(db, one_value, one_value)) {
+                        db_info = std::move(update_result.value());
+                    } else {
+                        state = db::manager_result_t{std::move(update_result.error())};
+                    }
                 } else {
-                    state = db::manager_result_t{std::move(purge_result.error())};
-                }
-
-                // infoをクリア。セーブIDを1にする
-                db::value const one_value = db::value{db::integer::type{1}};
-                if (auto update_result = db::update_db_info(db, one_value, one_value)) {
-                    db_info = std::move(update_result.value());
-                } else {
-                    state = db::manager_result_t{std::move(update_result.error())};
+                    state = std::move(purge_result);
                 }
 
                 // トランザクション終了
