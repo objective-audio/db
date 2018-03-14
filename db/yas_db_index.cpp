4245fb3ea1f3e4a304d17c65470107b5dba85d85
Yuki Yasoshima <yukiyasos@gmail.com>
2018-03-14 21:28:53 +0900

    update index

--- db/yas_db_index.cpp
+++ db/yas_db_index.cpp
@@ -7,13 +7,25 @@
 
 using namespace yas;
 
-namespace yas::db {
-static std::string const entity_key = "entity";
-static std::string const attributes_key = "attributes";
-}
-
 db::index::index(index_args args)
     : name(std::move(args.name)), entity(std::move(args.entity)), attributes(std::move(args.attributes)) {
+    if (this->name.size() == 0) {
+        throw std::invalid_argument("invalid name.");
+    }
+
+    if (this->entity.size() == 0) {
+        throw std::invalid_argument("invalid empty.");
+    }
+
+    if (this->attributes.size() == 0) {
+        throw std::invalid_argument("attributes is empty.");
+    }
+
+    for (auto const &attribute : this->attributes) {
+        if (attribute.size() == 0) {
+            throw std::invalid_argument("invalid attribute.");
+        }
+    }
 }
 
 std::string db::index::sql_for_create() const {
