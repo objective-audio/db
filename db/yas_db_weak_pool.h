a06d47c10dfce27e61e217e28288ff0bad837573
Yuki Yasoshima <yukiyasos@gmail.com>
2019-09-29 19:09:13 +0900

    remove weak_pool

--- db/yas_db_weak_pool.h
+++ db/yas_db_weak_pool.h
@@ -10,25 +10,6 @@
 
 namespace yas::db {
 template <typename K, typename V>
-struct weak_pool final {
-    using value_map_t = std::unordered_map<K, weak_ref<V>>;
-    using value_create_handler = std::function<V(void)>;
-    using perform_handler = std::function<void(std::string const &, K const &, V &)>;
-
-    V get_or_create(std::string const &entity_name, K const &key, value_create_handler handler);
-    std::optional<V> get(std::string const &entity_name, K const &key);
-    void set(std::string const &entity_name, K const &key, V value);
-
-    void perform(perform_handler const &handler);
-    void perform_entity(std::string const &entity_name, perform_handler const &handler);
-    void erase(std::string const &entity_name, K const &key);
-    void clear();
-
-   private:
-    std::unordered_map<std::string, value_map_t> _all_values;
-};
-
-template <typename K, typename V>
 struct weak_pool2 final {
     using value_ptr = std::shared_ptr<V>;
     using value_wptr = std::weak_ptr<V>;
