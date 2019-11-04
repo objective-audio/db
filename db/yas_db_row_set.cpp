098e3d5edf97cd9468345ec3c6e30574dbf4a990
Yuki Yasoshima <yukiyasos@gmail.com>
2019-11-04 10:35:50 +0900

    remove row_set impl

--- db/yas_db_row_set.cpp
+++ db/yas_db_row_set.cpp
@@ -9,7 +9,6 @@
 #include <vector>
 #include "yas_db_database.h"
 #include "yas_db_statement.h"
-#include "yas_db_value.h"
 
 using namespace yas;
 
@@ -22,93 +21,38 @@ db::next_result_code::operator bool() const {
     return this->raw_value() == SQLITE_ROW;
 }
 
-#pragma mark - impl
-
-struct db::row_set::impl {
-    impl(db::statement_ptr const &statement, database_ptr const &database, std::vector<db::value> const &context)
-        : _statement(statement), _database(database), _context(context) {
-        this->_statement->set_in_use(true);
-    }
-
-    ~impl() {
-        this->close();
-    }
-
-    uintptr_t identifier() {
-        return reinterpret_cast<uintptr_t>(this);
-    }
-
-    void close() {
-        this->_statement->reset();
-        if (this->_database) {
-            if (db::row_set_observable_ptr const observable_db = row_set_observable::cast(this->_database)) {
-                observable_db->row_set_did_close(identifier());
-            }
-            this->_database = nullptr;
-        }
-    }
-
-    void _set_database(database_ptr const &database) {
-        this->_database = database;
-    }
-
-    database_ptr const &database() const {
-        return this->_database;
-    }
-
-    db::statement_ptr const &statement() const {
-        return this->_statement;
-    }
-
-    std::unordered_map<std::string, int> const &column_name_to_index_map() const {
-        if (this->_column_name_to_index_map.empty()) {
-            sqlite3_stmt *const stmt = this->_statement->stmt();
-            int column_count = sqlite3_column_count(stmt);
-            auto each = make_fast_each(column_count);
-            while (yas_each_next(each)) {
-                int const &idx = yas_each_index(each);
-                this->_column_name_to_index_map.insert(std::make_pair(to_lower(sqlite3_column_name(stmt, idx)), idx));
-            }
-        }
-
-        return this->_column_name_to_index_map;
-    }
-
-   private:
-    db::database_ptr _database;
-    db::statement_ptr _statement;
-    std::vector<db::value> _context;
-
-    mutable std::unordered_map<std::string, int> _column_name_to_index_map;
-};
+#pragma mark - row_set
 
 db::row_set::row_set(db::statement_ptr const &statement, database_ptr const &database,
                      std::vector<db::value> const &context)
-    : _impl(std::make_unique<impl>(statement, database, context)) {
+    : _statement(statement), _database(database), _context(context) {
+    this->_statement->set_in_use(true);
 }
 
-db::row_set::~row_set() = default;
+db::row_set::~row_set() {
+    this->close();
+}
 
 uintptr_t db::row_set::identifier() const {
-    return this->_impl->identifier();
+    return reinterpret_cast<uintptr_t>(this);
 }
 
 db::statement_ptr const &db::row_set::statement() const {
-    return this->_impl->statement();
+    return this->_statement;
 }
 
 db::next_result_code db::row_set::next() {
-    next_result_code result{sqlite3_step(this->_impl->statement()->stmt())};
+    next_result_code result{sqlite3_step(this->statement()->stmt())};
 
     if (!result) {
-        this->_impl->close();
+        this->close();
     }
 
     return result;
 }
 
 bool db::row_set::has_row() {
-    if (db::database_ptr const &database = this->_impl->database()) {
+    if (db::database_ptr const &database = this->_database) {
         if (sqlite3 *const sqlite_handle = database->sqlite_handle()) {
             return sqlite3_errcode(sqlite_handle) == SQLITE_ROW;
         }
@@ -117,13 +61,13 @@ bool db::row_set::has_row() {
 }
 
 int db::row_set::column_count() const {
-    return sqlite3_column_count(this->_impl->statement()->stmt());
+    return sqlite3_column_count(this->statement()->stmt());
 }
 
 db::row_set::index_result_t db::row_set::column_index(std::string column_name) const {
     std::string lower_column_name = to_lower(std::move(column_name));
 
-    auto const &map = this->_impl->column_name_to_index_map();
+    auto const &map = this->_get_or_make_column_name_to_index_map();
 
     if (map.count(lower_column_name) > 0) {
         return db::row_set::index_result_t{map.at(lower_column_name)};
@@ -133,11 +77,11 @@ db::row_set::index_result_t db::row_set::column_index(std::string column_name) c
 }
 
 std::string db::row_set::column_name(int const column_idx) const {
-    return sqlite3_column_name(this->_impl->statement()->stmt(), column_idx);
+    return sqlite3_column_name(this->statement()->stmt(), column_idx);
 }
 
 bool db::row_set::column_is_null(int const column_idx) {
-    return sqlite3_column_type(this->_impl->statement()->stmt(), column_idx) == SQLITE_NULL;
+    return sqlite3_column_type(this->statement()->stmt(), column_idx) == SQLITE_NULL;
 }
 
 bool db::row_set::column_is_null(std::string column_name) {
@@ -149,7 +93,7 @@ bool db::row_set::column_is_null(std::string column_name) {
 
 db::value db::row_set::column_value(int const column_idx) const {
     if (column_idx >= 0) {
-        sqlite3_stmt *const stmt = this->_impl->statement()->stmt();
+        sqlite3_stmt *const stmt = this->statement()->stmt();
         int type = sqlite3_column_type(stmt, column_idx);
 
         if (type != SQLITE_NULL) {
@@ -179,7 +123,7 @@ db::value db::row_set::column_value(std::string column_name) const {
 }
 
 db::value_map_t db::row_set::values() const {
-    sqlite3_stmt *const stmt = this->_impl->statement()->stmt();
+    sqlite3_stmt *const stmt = this->statement()->stmt();
     int const column_count = sqlite3_data_count(stmt);
 
     db::value_map_t map;
@@ -195,11 +139,31 @@ db::value_map_t db::row_set::values() const {
 }
 
 void db::row_set::close() {
-    this->_impl->close();
+    this->_statement->reset();
+    if (this->_database) {
+        if (db::row_set_observable_ptr const observable_db = row_set_observable::cast(this->_database)) {
+            observable_db->row_set_did_close(identifier());
+        }
+        this->_database = nullptr;
+    }
 }
 
 void db::row_set::set_database(database_ptr const &database) {
-    this->_impl->_set_database(database);
+    this->_database = database;
+}
+
+std::unordered_map<std::string, int> const &db::row_set::_get_or_make_column_name_to_index_map() const {
+    if (this->_column_name_to_index_map.empty()) {
+        sqlite3_stmt *const stmt = this->_statement->stmt();
+        int column_count = sqlite3_column_count(stmt);
+        auto each = make_fast_each(column_count);
+        while (yas_each_next(each)) {
+            int const &idx = yas_each_index(each);
+            this->_column_name_to_index_map.insert(std::make_pair(to_lower(sqlite3_column_name(stmt, idx)), idx));
+        }
+    }
+
+    return this->_column_name_to_index_map;
 }
 
 db::row_set_ptr db::row_set::make_shared(db::statement_ptr const &statement, database_ptr const &database,
