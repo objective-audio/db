6a778561eea1ae87878dd0cbda549b8760a0bfb7
Yuki Yasoshima <yukiyasos@gmail.com>
2020-03-13 20:27:20 +0900

    add error tests

--- db/yas_db_error.cpp
+++ db/yas_db_error.cpp
@@ -18,13 +18,13 @@ db::sqlite_result_code::operator bool() const {
 
 #pragma mark - error
 
-db::error::error(db::error_type const type) : db::error(type, 0) {
+db::error::error(db::error_type const type) : db::error(type, SQLITE_OK) {
 }
 
 db::error::error(db::error_type const type, db::sqlite_result_code const &code) : db::error(type, code, "") {
 }
 
-db::error::error(std::nullptr_t) : _type(db::error_type::none), _code(0), _message() {
+db::error::error(std::nullptr_t) : _type(db::error_type::none), _code(SQLITE_OK), _message() {
 }
 
 db::error::error(db::error_type const type, db::sqlite_result_code const &code, std::string message)
---
+++ db_tests/yas_db_error_tests.mm
@@ -0,0 +1,64 @@
+//
+//  yas_db_error_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import "yas_db_test_utils.h"
+
+using namespace yas;
+
+@interface yas_db_error_tests : XCTestCase
+
+@end
+
+@implementation yas_db_error_tests
+
+- (void)test_construct_with_type_only {
+    db::error error{db::error_type::sqlite};
+    
+    XCTAssertEqual(error.type(), db::error_type::sqlite);
+    XCTAssertEqual(error.code().raw_value(), SQLITE_OK);
+    XCTAssertEqual(error.message(), "");
+}
+
+- (void)test_construct_without_message {
+    db::error error{db::error_type::in_use, db::sqlite_result_code{SQLITE_INTERNAL}};
+    
+    XCTAssertEqual(error.type(), db::error_type::in_use);
+    XCTAssertEqual(error.code().raw_value(), SQLITE_INTERNAL);
+    XCTAssertEqual(error.message(), "");
+}
+
+- (void)test_construct_with_all_parameters {
+    db::error error{db::error_type::closed, db::sqlite_result_code{SQLITE_ERROR}, "test_message"};
+    
+    XCTAssertEqual(error.type(), db::error_type::closed);
+    XCTAssertEqual(error.code().raw_value(), SQLITE_ERROR);
+    XCTAssertEqual(error.message(), "test_message");
+}
+
+- (void)test_construct_with_nullptr {
+    db::error error{nullptr};
+    
+    XCTAssertEqual(error.type(), db::error_type::none);
+    XCTAssertEqual(error.code().raw_value(), SQLITE_OK);
+    XCTAssertEqual(error.message(), "");
+}
+
+- (void)test_bool {
+    XCTAssertFalse(db::error(db::error_type::none));
+    
+    XCTAssertTrue(db::error(db::error_type::closed));
+    XCTAssertTrue(db::error(db::error_type::in_use));
+    XCTAssertTrue(db::error(db::error_type::invalid_query_count));
+    XCTAssertTrue(db::error(db::error_type::invalid_argument));
+    XCTAssertTrue(db::error(db::error_type::sqlite));
+}
+
+- (void)test_error_to_string {
+    db::error error{db::error_type::closed};
+    
+    to_string(error);
+}
+
+@end
