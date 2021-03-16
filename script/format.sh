44721ff7926c34ddfd06e7a882d92d4ebdec490e
Yuki Yasoshima <yukiyasos@gmail.com>
2021-03-16 23:44:37 +0900

    fix format.sh

--- script/format.sh
+++ script/format.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+export PATH=$PATH:/opt/homebrew/bin
 clang-format -i -style=file `find ../db ../db_ios ../db_test -type f \( -name *.h -o -name *.cpp -o -name *.hpp -o -name *.m -o -name *.mm \)`
