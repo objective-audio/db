73ae4f65baa83a180f2e5fb42d1ae8d14b104b34
Yuki Yasoshima <yukiyasos@gmail.com>
2021-04-10 19:52:06 +0900

    fix database

--- db/core/yas_db_database.h
+++ db/core/yas_db_database.h
@@ -25,21 +25,21 @@ struct database final : row_set_observable {
 
     using callback_f = std::function<int(db::value_map_t const &)>;
 
-    static std::string sqlite_lib_version();
-    static bool sqlite_thread_safe();
+    [[nodiscard]] static std::string sqlite_lib_version();
+    [[nodiscard]] static bool sqlite_thread_safe();
 
     ~database();
 
-    std::string const &database_path() const;
-    sqlite3 *sqlite_handle() const;
+    [[nodiscard]] std::string const &database_path() const;
+    [[nodiscard]] sqlite3 *sqlite_handle() const;
 
     bool open();
 #if SQLITE_VERSION_NUMBER >= 3005000
     bool open(int flags);
 #endif
     void close();
-    bool good_connection() const;
-    db::integrity_result_t integrity_check() const;
+    [[nodiscard]] bool good_connection() const;
+    [[nodiscard]] db::integrity_result_t integrity_check() const;
 
     db::update_result_t execute_update(std::string const &sql);
     db::update_result_t execute_update(std::string const &sql, db::value_vector_t const &arguments);
@@ -47,30 +47,30 @@ struct database final : row_set_observable {
 
     db::update_result_t execute_statements(std::string const &sql);
     db::update_result_t execute_statements(std::string const &sql, callback_f const &callback);
-    callback_f const &callback_for_execute_statements() const;
+    [[nodiscard]] callback_f const &callback_for_execute_statements() const;
 
     db::query_result_t execute_query(std::string const &sql) const;
     db::query_result_t execute_query(std::string const &sql, db::value_vector_t const &arguments) const;
     db::query_result_t execute_query(std::string const &sql, db::value_map_t const &arguments) const;
 
-    db::row_result_t last_insert_rowid() const;
-    db::count_result_t changes() const;
+    [[nodiscard]] db::row_result_t last_insert_rowid() const;
+    [[nodiscard]] db::count_result_t changes() const;
 
     void clear_cached_statements();
     void close_opened_row_sets();
-    bool has_opened_row_sets() const;
-    bool should_cache_statements() const;
+    [[nodiscard]] bool has_opened_row_sets() const;
+    [[nodiscard]] bool should_cache_statements() const;
     void set_should_cache_statements(bool flag);
 
-    std::string last_error_message() const;
-    int last_error_code() const;
-    bool had_error() const;
+    [[nodiscard]] std::string last_error_message() const;
+    [[nodiscard]] int last_error_code() const;
+    [[nodiscard]] bool had_error() const;
     void set_max_busy_retry_time_interval(double const);
-    double max_busy_retry_time_interval() const;
+    [[nodiscard]] double max_busy_retry_time_interval() const;
     void set_start_busy_retry_time(const std::chrono::time_point<std::chrono::system_clock> &time);
-    std::chrono::time_point<std::chrono::system_clock> start_busy_retry_time() const;
+    [[nodiscard]] std::chrono::time_point<std::chrono::system_clock> start_busy_retry_time() const;
 
-    static database_ptr make_shared(std::string const &path);
+    [[nodiscard]] static database_ptr make_shared(std::string const &path);
 
    private:
     uint8_t _db_key;
